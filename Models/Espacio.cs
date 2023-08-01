fc074327976c0079f9ce234fe86f172e12eea4cc
Javier Uroz <urozjavier@gmail.com>
2023-07-31 21:31:15 -0300

    cambios JU

--- Models/Espacio.cs
+++ Models/Espacio.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel.DataAnnotations;
 
-namespace Fundacion.Models;
+//namespace Fundacion.Models;
 
-public partial class Espacio
-{
-    [Key]
-    public int EsId { get; set; }
+//public partial class Espacio
+//{
+//    [Key]
+//    public int EsId { get; set; }
 
-    public string EsDescripcion { get; set; } = null!;
+//    public string EsDescripcion { get; set; } = null!;
 
-    public int AuId { get; set; }
+//    public int AuId { get; set; }
 
-    public string EsDia { get; set; } = null!;
+//    public string EsDia { get; set; } = null!;
 
-    public string EsHora { get; set; } = null!;
+//    public string EsHora { get; set; } = null!;
 
-    public double EsCantHs { get; set; }
+//    public double EsCantHs { get; set; }
 
-    public int TuId { get; set; }
+//    public int TuId { get; set; }
 
-    public int UsId { get; set; }
+//    public int UsId { get; set; }
 
-    //public string EsActivo { get; set; }
+//    //public string EsActivo { get; set; }
 
-    public bool EsActivo { get; set; }
+//    public bool EsActivo { get; set; }
 
-    public int CaId { get; set; }
+//    public int CaId { get; set; }
 
-    public virtual ICollection<Asistencia> Asistencia { get; set; } = new List<Asistencia>();
+//    public virtual ICollection<Asistencia> Asistencia { get; set; } = new List<Asistencia>();
 
-    public virtual Aula? Au { get; set; } = null!;
+//    public virtual Aula? Au { get; set; } = null!;
 
-    public virtual Categoria? Ca { get; set; }
+//    public virtual Categoria? Ca { get; set; }
 
-    public virtual Turno? Tu { get; set; } = null!;
+//    public virtual Turno? Tu { get; set; } = null!;
 
-    public virtual Usuario? Us { get; set; } = null!;
-}
+//    public virtual Usuario? Us { get; set; } = null!;
+//}
 
 ﻿using System;
 using System.Collections.Generic;
