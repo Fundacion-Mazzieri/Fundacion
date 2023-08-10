b63189b674ce396b88d5b67d5777f82d4cb507b0
MarianGmz <marianogomezdev@gmail.com>
2023-08-09 21:17:49 -0300

    Agregando controlador Turno

---
+++ Controllers/TurnoController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fundacion.Data;
+using Fundacion.Models;
+
+namespace Fundacion.Controllers
+{
+    public class TurnoController : Controller
+    {
+        private readonly FundacionContext _context;
+
+        public TurnoController(FundacionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Turno
+        public async Task<IActionResult> Index()
+        {
+              return _context.Turnos != null ? 
+                          View(await _context.Turnos.ToListAsync()) :
+                          Problem("Entity set 'FundacionContext.Turnos'  is null.");
+        }
+
+        // GET: Turno/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .FirstOrDefaultAsync(m => m.TuId == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // GET: Turno/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Turno/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TuId,TuDescripcion")] Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turno);
+        }
+
+        // GET: Turno/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            return View(turno);
+        }
+
+        // POST: Turno/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TuId,TuDescripcion")] Turno turno)
+        {
+            if (id != turno.TuId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurnoExists(turno.TuId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(turno);
+        }
+
+        // GET: Turno/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .FirstOrDefaultAsync(m => m.TuId == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // POST: Turno/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Turnos == null)
+            {
+                return Problem("Entity set 'FundacionContext.Turnos'  is null.");
+            }
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno != null)
+            {
+                _context.Turnos.Remove(turno);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurnoExists(int id)
+        {
+          return (_context.Turnos?.Any(e => e.TuId == id)).GetValueOrDefault();
+        }
+    }
+}
