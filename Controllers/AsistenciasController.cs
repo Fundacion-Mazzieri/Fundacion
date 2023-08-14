067b14eb4c99eec1998a0671395763051a4c2a26
Matias-Mobili <matiasmobili889@gmail.com>
2023-08-14 20:00:56 -0300

    Asistencia

---
+++ Controllers/AsistenciasController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fundacion.Data;
+using Fundacion.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Fundacion.Controllers
+{
+    public class AsistenciasController : Controller
+    {
+        private readonly FundacionContext _context;
+
+        public AsistenciasController(FundacionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Asistencias
+        public async Task<IActionResult> Index()
+        {
+            var fundacionContext = _context.Asistencias.Include(a => a.Es);
+            return View(await fundacionContext.ToListAsync());
+        }
+
+        // GET: Asistencias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Asistencias == null)
+            {
+                return NotFound();
+            }
+
+            var asistencia = await _context.Asistencias
+                .Include(a => a.Es)
+                .FirstOrDefaultAsync(m => m.AsiId == id);
+            if (asistencia == null)
+            {
+                return NotFound();
+            }
+
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Create
+        public IActionResult Create()
+        {
+            ViewData["EsId"] = new SelectList(_context.Set<Espacio>(), "EsId", "EsDescripcion");
+            return View();
+        }
+
+        // POST: Asistencias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AsiId,EsId,AsIngreso,AsEgreso,AsPresent")] Asistencia asistencia)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(asistencia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EsId"] = new SelectList(_context.Set<Espacio>(), "EsId", "EsDescripcion", asistencia.EsId);
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Asistencias == null)
+            {
+                return NotFound();
+            }
+
+            var asistencia = await _context.Asistencias.FindAsync(id);
+            if (asistencia == null)
+            {
+                return NotFound();
+            }
+            ViewData["EsId"] = new SelectList(_context.Set<Espacio>(), "EsId", "EsDescripcion", asistencia.EsId);
+            return View(asistencia);
+        }
+
+        // POST: Asistencias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AsiId,EsId,AsIngreso,AsEgreso,AsPresent")] Asistencia asistencia)
+        {
+            if (id != asistencia.AsiId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asistencia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AsistenciaExists(asistencia.AsiId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EsId"] = new SelectList(_context.Set<Espacio>(), "EsId", "EsDescripcion", asistencia.EsId);
+            return View(asistencia);
+        }
+
+        // GET: Asistencias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Asistencias == null)
+            {
+                return NotFound();
+            }
+
+            var asistencia = await _context.Asistencias
+                .Include(a => a.Es)
+                .FirstOrDefaultAsync(m => m.AsiId == id);
+            if (asistencia == null)
+            {
+                return NotFound();
+            }
+
+            return View(asistencia);
+        }
+
+        // POST: Asistencias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Asistencias == null)
+            {
+                return Problem("Entity set 'FundacionContext.Asistencia'  is null.");
+            }
+            var asistencia = await _context.Asistencias.FindAsync(id);
+            if (asistencia != null)
+            {
+                _context.Asistencias.Remove(asistencia);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AsistenciaExists(int id)
+        {
+            return (_context.Asistencias?.Any(e => e.AsiId == id)).GetValueOrDefault();
+        }
+    }
+}
--- Models/Asistencia.cs
+++ Models/Asistencia.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fundacion.Models;
 
 public partial class Asistencia
 {
+    [Key]
     public int AsiId { get; set; }
-
     public int EsId { get; set; }
 
     public DateTime? AsIngreso { get; set; }
@@ -15,5 +17,5 @@ public partial class Asistencia
 
     public bool AsPresent { get; set; }
 
-    public virtual Espacio Es { get; set; } = null!;
+    public virtual Espacio? Es { get; set; } = null!;
 }
