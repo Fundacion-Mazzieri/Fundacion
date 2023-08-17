2651e4f5fc8b6a3417e03e41c1ea049a15b026d5
JavaUroz <urozjavier@gmail.com>
2023-08-17 12:57:28 -0300

    asistencias JU

--- Models/Asistencia.cs
+++ Models/Asistencia.cs
@@ -10,9 +10,11 @@ public partial class Asistencia
     [Key]
     public int AsiId { get; set; }
     public int EsId { get; set; }
-
+    [DataType(DataType.DateTime)]    
+    [Required(ErrorMessage = "Debe ingresar una fecha y hora de ingreso.")]
     public DateTime? AsIngreso { get; set; }
-
+    [DataType(DataType.DateTime)]
+    [Required(ErrorMessage = "Debe ingresar una fecha y hora de egreso.")]
     public DateTime? AsEgreso { get; set; }
 
     public bool AsPresent { get; set; }
