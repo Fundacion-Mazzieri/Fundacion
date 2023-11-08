e9365d6e35faa70b80ca6604d56f20d18d749c0f
Javier Uroz <urozjavier@gmail.com>
2023-11-08 19:35:34 -0300

    quitar restricciones a pass crear usuario JU

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -44,7 +44,7 @@ public partial class Usuario
     public long? UsTelefono { get; set; }    
     [Display(Name = "Contraseña")]
     [PasswordPropertyText(true)]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d']).+$", ErrorMessage = "La contraseña debe tener al menos una minúscula, una mayúscula, un número y un símbolo.")]
+    //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d']).+$", ErrorMessage = "La contraseña debe tener al menos una minúscula, una mayúscula, un número y un símbolo.")]
     [Required(ErrorMessage = "Debe ingresar una contraseña.")]
     [MinLength(8, ErrorMessage = "No se permiten menos de 8 caracteres.")]
     [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres.")]
